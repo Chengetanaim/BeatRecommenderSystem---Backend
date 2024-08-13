e0b7a1591348c8e8b1b7571027ce1298293e1694
chengetanai <mukanhairic@gmail.com>
2024-08-13 15:25:56 +0200

    use environment variables for database connection

--- app/database.py
+++ app/database.py
@@ -1,14 +1,12 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-
+from .config import settings
 
 Base = declarative_base()
 
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://postgres:admin@localhost:5432/beat-recommender-system"
-)
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.username}:{settings.password}@{settings.host_name}/{settings.db_name}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
